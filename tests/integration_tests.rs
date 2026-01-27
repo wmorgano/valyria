c516281a228e8705d4de15be50b6e835e3d5759a
Will Morgano <wtcmorgano@gmail.com>
2026-01-27 12:14:43 -0800

    Refactor integration_tests to use functional approach with `copied` and `unzip` methods.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,19 +4,20 @@ fn get_repl_test_command() -> assert_cmd::Command {
     cargo::cargo_bin_cmd!()
 }
 
-fn make_repl_input(repl_cmds: &Vec<&str>) -> String {
+fn make_repl_input(repl_cmds: &[&str]) -> String {
     repl_cmds.join("\n") + "\nquit"
 }
 
-fn make_repl_outputs(repl_cmds: &Vec<&str>) -> String {
+fn make_repl_outputs(repl_cmds: &[&str]) -> String {
     let mut outputs = String::from(">> ");
     outputs.push_str(&repl_cmds.join("\n>> "));
     outputs.push_str("\n>> ");
     outputs
 }
 
-fn make_repl_test_io(repl_cmds: Vec<&str>) -> (String, String) {
-    (make_repl_input(&repl_cmds), make_repl_outputs(&repl_cmds))
+fn make_repl_test_io(repl_cmds: &[(&str, &str)]) -> (String, String) {
+    let (inputs, outputs): (Vec<&str>, Vec<&str>) = repl_cmds.iter().copied().unzip();
+    (make_repl_input(&inputs), make_repl_outputs(&outputs))
 }
 
 #[test]
@@ -29,7 +30,7 @@ fn run_repl() {
 #[test]
 fn run_repl_with_integer_input() {
     let mut cmd = get_repl_test_command();
-    let (repl_input, repl_output) = make_repl_test_io(vec!["123"]);
+    let (repl_input, repl_output) = make_repl_test_io(&[("123", "123")]);
     let assert = cmd.write_stdin(repl_input).assert();
     assert.stdout(repl_output).success();
 }
