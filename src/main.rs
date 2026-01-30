8833fc3fc309c6e9fdf94265ce7014337aef7c2c
Will Morgano <william.morgano@intel.com>
2026-01-30 10:58:04 -0800

    Refactor REPL.
    - Use `&str` reference in place of owned `String`
    - Use `if let` instead of matching with `None => ()`
    - Use `Option::map` instead of matching with `None => None`
    - Change `tokenizer` function to `parse` which splits on whitespace an
      creates a token for each element

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io::{self, Write};
 
-fn display_prompt(prompt: String, writer: &mut impl Write) {
-    write!(writer, "{}", prompt).expect("Failed to write prompt.");
+fn display_prompt(prompt: &str, writer: &mut impl io::Write) {
+    write!(writer, "{prompt}").expect("Failed to write prompt.");
 }
 
-fn read_expression(writer: &mut impl Write, reader: &mut impl io::BufRead) -> Vec<String> {
-    let prompt = String::from(">> ");
+fn read_expression(writer: &mut impl io::Write, reader: &mut impl io::BufRead) -> String {
+    let prompt = ">> ";
     display_prompt(prompt, writer);
     writer
         .flush()
@@ -17,14 +17,11 @@ fn read_expression(writer: &mut impl Write, reader: &mut impl io::BufRead) -> Ve
         .read_line(&mut input)
         .expect("Failed to parse input.");
 
-    input.split_whitespace().map(String::from).collect()
+    input.trim().to_owned()
 }
 
-fn is_terminator(expression: &[String]) -> bool {
-    match expression.first() {
-        None => false,
-        Some(command) => command == "quit",
-    }
+fn is_terminator(expression: &str) -> bool {
+    expression == "quit"
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -33,26 +30,22 @@ struct Token {
 }
 
 impl Token {
-    fn new(literal: String) -> Self {
-        Token { literal }
+    fn new(literal: &str) -> Token {
+        Token {
+            literal: String::from(literal),
+        }
     }
 }
 
-fn tokenize(expression: &[String]) -> Vec<Token> {
-    expression
-        .iter()
-        .map(|token| Token::new(token.clone()))
-        .collect()
+fn parse(expression: &str) -> Vec<Token> {
+    expression.split_whitespace().map(Token::new).collect()
 }
 
-fn evaluate(expression: &Vec<String>) -> Result<String, &'static str> {
-    let tokens = tokenize(expression);
+fn evaluate(expression: &str) -> Option<String> {
+    let tokens = parse(expression);
 
     // For now, 'evaluation' will consist of just returning the first token.
-    match tokens.first() {
-        None => Err("Tokenized expression is empty."),
-        Some(result) => Ok(result.literal.clone()),
-    }
+    tokens.first().map(|result| result.literal.clone())
 }
 
 fn repl(mut writer: impl Write, mut reader: impl io::BufRead) {
@@ -62,9 +55,8 @@ fn repl(mut writer: impl Write, mut reader: impl io::BufRead) {
             break;
         }
 
-        match evaluate(&expression) {
-            Ok(result) => println!("{result}"),
-            Err(e) => println!("{}", e),
+        if let Some(result) = evaluate(&expression) {
+            println!("{result}");
         }
     }
 }
@@ -80,7 +72,7 @@ mod tests {
     #[test]
     fn test_display_prompt() {
         let mut output: Vec<u8> = Vec::new();
-        display_prompt(String::from(">> "), &mut output);
+        display_prompt(">> ", &mut output);
         assert_eq!(output, b">> ");
     }
 
@@ -90,42 +82,39 @@ mod tests {
         let mut input = "123".as_bytes();
         let mut output: Vec<u8> = Vec::new();
         let expression = read_expression(&mut output, &mut input);
-        assert_eq!(expression, vec!["123"]);
+        assert_eq!(expression, "123");
 
         // with whitespace to split
         let mut input = "123 456".as_bytes();
         let mut output: Vec<u8> = Vec::new();
         let expression = read_expression(&mut output, &mut input);
-        assert_eq!(expression, vec!["123", "456"]);
+        assert_eq!(expression, "123 456");
     }
 
     #[test]
     fn test_is_terminator() {
-        assert!(is_terminator(&vec![String::from("quit")]));
-        assert!(!is_terminator(&vec![String::from("exit")]));
+        assert!(is_terminator("quit"));
+        assert!(!is_terminator("exit"));
     }
 
     #[test]
     fn test_token() {
-        let token = Token::new(String::from("123"));
+        let token = Token::new("123");
         assert_eq!(token.literal, "123");
     }
 
     #[test]
-    fn test_tokenize() {
-        let input_expressions = vec![String::from("123"), String::from("456")];
+    fn test_parse() {
+        let input_expressions = "123 456";
 
-        let tokens = vec![
-            Token::new(String::from("123")),
-            Token::new(String::from("456")),
-        ];
+        let tokens = vec![Token::new("123"), Token::new("456")];
 
-        assert_eq!(tokenize(&input_expressions), tokens);
+        assert_eq!(parse(&input_expressions), tokens);
     }
 
     #[test]
     fn test_evaluate() {
-        let input_expressions = vec![String::from("123"), String::from("456")];
-        assert_eq!(evaluate(&input_expressions), Ok(String::from("123")));
+        let input_expressions = "123 456";
+        assert_eq!(evaluate(&input_expressions), Some(String::from("123")));
     }
 }
